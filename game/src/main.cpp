04f11cdbd529349a2a8da76d532894d50b4d60fa
unkemptBookworm <153551529+LidsvilleDev@users.noreply.github.com>
2025-09-12 16:53:24 -0400

    Added two circles

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,25 +7,28 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
-const unsigned int TARGET_FPS = 50;
-float time = 0;
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
-    SetTargetFPS(TARGET_FPS);
+    InitWindow(800, 800, "Physics-1");
+
+    Vector2 circle_position_1 = { 400.0f, 400.0f };
+    Vector2 circle_position_2 = { 400.0f, 400.0f };
+    float circle_radius = 25.0f;
+    Color circle_color = PURPLE;
 
     while (!WindowShouldClose())
     {
+        float t = GetTime();
+        float dt = GetFrameTime();
+        circle_position_1.x = 400.0f + 150.0f * sinf(t);
+        circle_position_2.y += 10.0f * dt;
         BeginDrawing();
             ClearBackground(WHITE);
             DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
+            DrawCircleV(circle_position_1, circle_radius, circle_color);
+            DrawCircleV(circle_position_2, circle_radius, circle_color);
         EndDrawing();
     }
 
